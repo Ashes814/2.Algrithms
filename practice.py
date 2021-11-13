7ce956bd8a40ee6f1605567990c79142b5374250
张欧文 <oo@zhangouwens-MacBook-Pro.local>
2021-11-13 21:13:50 +0800

    Initial commit

---
+++ practice.py
@@ -0,0 +1,13 @@
+def sum_4_1(arr):
+    if len(arr) == 0:
+        return 0
+    else:
+
+        result = arr[0] + sum_4_1(arr[1:])
+    return result
+
+
+print(sum_4_1([1, 2, 3, 4, 5,24,24,224,24,24]))
+
+
+#add
