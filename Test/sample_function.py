45a00575a1360e0f3ad171e85095da7856c715d6
Owen <1534661514@qq.com>
2022-03-10 11:44:01 +0800

    Implement test file

---
+++ Test/sample_function.py
@@ -0,0 +1,6 @@
+# this function is used for calculate the square of an positive number
+
+
+def square_num(num):
+
+    return num ** 2
---
+++ Test/test_function.py
@@ -0,0 +1,16 @@
+from sample_function import square_num
+
+
+def test_1():
+    assert(square_num(10) == 10**2)
+
+
+def test_1():
+    assert(square_num(10) == 10**2)
+def test_2():
+    assert(square_num(10) == 10**2)
+def test_5():
+    assert(square_num(10) == 10**2)
+def test_3():
+    assert(square_num(10) == 10**2)
+
